9de66e31e5c0c3b24c2853b8a7cfc03bad02896c
ProfAnanas <andrea.tarditi87@gmail.com>
2026-03-09 09:32:26 +0100

    bugfix 1.2.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NOME_CACHE = 'bobbiolab-cache-v1.2';
+const NOME_CACHE = 'bobbiolab-cache-v1.2.1';
 
 // Elenco dei file base da salvare per far funzionare l'interfaccia offline
 const FILE_DA_SALVARE = [
