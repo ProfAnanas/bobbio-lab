7d80bc53a74289ae2a74852f1f30d60c9bf9a7be
ProfAnanas <andrea.tarditi87@gmail.com>
2026-03-13 19:18:15 +0100

    1.4.1 fix responsive

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NOME_CACHE = 'bobbiolab-cache-v1.4.0';
+const NOME_CACHE = 'bobbiolab-cache-v1.4.1';
 
 // Elenco dei file base da salvare per far funzionare l'interfaccia offline
 const FILE_DA_SALVARE = [
