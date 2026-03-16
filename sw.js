a0a3a7d83530f88216afaa967336b593796b8ca4
ProfAnanas <andrea.tarditi87@gmail.com>
2026-03-16 09:23:31 +0100

    v1.5..8 fix bottone
    v1.5..8 fix bottone

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NOME_CACHE = 'bobbiolab-cache-v1.5.7';
+const NOME_CACHE = 'bobbiolab-cache-v1.5.8';
 
 // Elenco dei file base da salvare per far funzionare l'interfaccia offline
 const FILE_DA_SALVARE = [
