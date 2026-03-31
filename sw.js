1f9be8df8b2a36e3487e272aee155da7f37d9ec0
ProfAnanas <andrea.tarditi87@gmail.com>
2026-03-31 19:28:37 +0200

    v1.6.9.2
    fix grafica

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NOME_CACHE = 'bobbiolab-cache-v1.6.9.1';
+const NOME_CACHE = 'bobbiolab-cache-v1.6.9.2';
 
 // Elenco dei file base da salvare per far funzionare l'interfaccia offline
 const FILE_DA_SALVARE = [
